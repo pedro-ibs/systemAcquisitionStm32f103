c984b75000ca29857eec1b15ce0137703e57e5ed
pedro_ibs <pedro.igor.ifsp@gmail.com>
2021-03-12 16:15:41 -0300

    add Licença

--- systemAcquisition/include/FreeRTOS/Drivers/rtc.h
+++ systemAcquisition/include/FreeRTOS/Drivers/rtc.h
@@ -8,7 +8,21 @@
  * 	tabSize:	8
  *
  * ########################################################
- * TODO: Licence
+ *   Copyright (C) Pedro Igor B. S 2021
+ * -------------------------------------------------------------------
+ *
+ *   Licença: GNU GPL 2
+ * -------------------------------------------------------------------
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License as
+ *   published by the Free Software Foundation; version 2 of the
+ *   License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * -------------------------------------------------------------------
  * ########################################################
  *
  * Controla a periferia de hardware RCT. essa biblioteca
